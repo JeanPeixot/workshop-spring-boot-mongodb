c9f6d435df02a65c6b644f1d9ee4eb4ee1986144
Jean Peixoto <jeanpeixoto70@gmail.com>
2024-11-19 08:32:09 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/jeanpeixoto/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanpeixoto.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
