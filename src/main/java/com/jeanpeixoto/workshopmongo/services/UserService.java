59a5fcff17de978b64f94e2048e092f9a133b08c
Jean Peixoto <jeanpeixoto70@gmail.com>
2024-11-19 10:02:26 -0300

    Obtendo um usuario por id

--- src/main/java/com/jeanpeixoto/workshopmongo/services/UserService.java
+++ src/main/java/com/jeanpeixoto/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jeanpeixoto.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeanpeixoto.workshopmongo.domain.User;
 import com.jeanpeixoto.workshopmongo.repository.UserRepository;
+import com.jeanpeixoto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,12 @@ public class UserService {
 		return repo.findAll();
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+		}
+	}
 
-}
+
